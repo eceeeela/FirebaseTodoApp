82732c7027419f3851b6b5e5307799711df54cf1
Yiling Chen <yiling.cn@gmail.com>
2024-11-03 13:09:52 -0500

    Initial Commit

---
+++ FirebaseTodo/FirebaseTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTodoApp.swift
+//  FirebaseTodo
+//
+//  Created by Elaine Chen on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
